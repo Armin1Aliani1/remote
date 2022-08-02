28fa61bb7a46f0845b0f23f9fd3b48346bc646c5
Dragon2623 <armin1.aliani1@gmail.com>
2022-08-02 11:45:57 +0430

    add NotThread

---
+++ src/main/java/com/homeWorks/thirteen/NotThread.java
@@ -0,0 +1,19 @@
+package com.homeWorks.thirteen;
+
+import java.util.Date;
+
+public class NotThread implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("Hello : " + new Date());
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("Stop Hello : " + new Date());
+                return;
+            }
+        }
+    }
+
+}
